86062ade0708f48e4d4e43a6740d8b8a37f56777
Tharun L <106159594+Tharunlakshmanan@users.noreply.github.com>
2023-09-12 13:28:25 +0530

    added about, contact,blog page using jinja techniq
    Signed-off-by: Tharun L <106159594+Tharunlakshmanan@users.noreply.github.com>

--- portfolioProject/portfolioApp/urls.py
+++ portfolioProject/portfolioApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from portfolioApp import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
+    path('blog',views.blog,name='blog')
 ]
