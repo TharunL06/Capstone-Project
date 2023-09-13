43ffb070ab8c6cc23de677ebcf8f03c88c64085e
Tharun L <106159594+Tharunlakshmanan@users.noreply.github.com>
2023-09-14 00:26:39 +0530

    created signup and login page static alert msg dyn
    Signed-off-by: Tharun L <106159594+Tharunlakshmanan@users.noreply.github.com>

--- portfolioProject/portfolioApp/views.py
+++ portfolioProject/portfolioApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request,"index.html")
@@ -17,4 +17,12 @@ def handlelogin(request):
     return render(request,"login.html")
 
 def signup(request):
+    if request.method=="POST":
+        fname=request.POST.get('fname')
+        lname=request.POST.get('lname')
+        email=request.POST.get('email')
+        pass1=request.POST.get('pass2')
+        pass2=request.POST.get('pass2')
+        print(fname,lname,email,pass1,pass2)
+
     return render(request,"signup.html")
--- portfolioProject/portfolioProject/settings.py
+++ portfolioProject/portfolioProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.ERROR:'danger'
+}
+ 
